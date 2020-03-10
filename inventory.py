9f13b662cc61afdcdc3294db74cc19f28377abfb
Colin Dietrich <repos@wildjuniper.com>
2020-03-10 10:52:55 -0700

    Fix inventory.py

--- inventory.py
+++ inventory.py
@@ -1,25 +1,35 @@
 """Inventory all data output"""
 
-import config, tools
+import config, tools, os
 
-print("TPC-DS")
-print("======")
+print("TPC-DS Inventory")
+print("================")
 print()
-_base = config.fp_ds_output_mnt + config.sep
-filepath_list = [_base + "1GB",
-                 _base + "1GBA",
-                 _base + "1GBB"]
+
+_base_local = config.fp_ds_output + config.sep
+_base_mnt = config.fp_ds_output_mnt + config.sep
+
+filepath_list = []
+for base in [_base_local, _base_mnt]:
+    for size in config.tpc_scale:
+        filepath_list.append(base + str(size) + "GB")
+
 for fp in filepath_list:
     if os.path.exists(fp):
         tools.print_inventory(fp)
     
-print("TPC-H")
-print("=====")
+print("TPC-H Inventory")
+print("===============")
 print()
-_base = config.fp_ds_output_mnt + config.sep
-filepath_list = [_base + "1GB",
-                 _base + "1GBA",
-                 _base + "1GBB"]
+
+_base_local = config.fp_h_output + config.sep
+_base_mnt = config.fp_h_output_mnt + config.sep
+
+filepath_list = []
+for base in [_base_local, _base_mnt]:
+    for size in config.tpc_scale:
+        filepath_list.append(base + str(size) + "GB")
+
 for fp in filepath_list:
     if os.path.exists(fp):
         tools.print_inventory(fp)
