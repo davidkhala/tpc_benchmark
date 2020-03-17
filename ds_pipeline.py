06fa5f9b0f4a116f1f9aeab3aa67aac892efa2d3
Colin Dietrich <repos@wildjuniper.com>
2020-03-17 10:42:09 -0700

    refactor ds and h setup file names

--- ds_pipeline.py
+++ ds_pipeline.py
@@ -4,20 +4,20 @@ Downloads, makes and generated bash scripts to generate
 TPC-DS data for benchmarking.
 """
 
-import config, tpcds_setup, tpcds_bq 
+import config, ds_setup, tpcds_bq
 
 # create project folders
-tpcds_setup.make_directories()
+ds_setup.make_directories()
 
 # download tpc-ds and tpc-h zipfiles
-tpcds_setup.download_zip()
+ds_setup.download_zip()
 
 # extract tpc-ds zip
-tpcds_setup.extract_tpcds_zip(zip_filepath=config.fp_ds_zip,
-                              version=config.fp_ds_src_version)
+ds_setup.extract_tpcds_zip(zip_filepath=config.fp_ds_zip,
+                           version=config.fp_ds_src_version)
 
 # run tpc-ds make for linux
-tpcds_setup.make_tpcds()
+ds_setup.make_tpcds()
 
 # edit the tpc-ds schema file
 tpcds_bq.rewrite_schema(filepath_in=config.tpcds_schema_ansi_sql_filepath, 
@@ -27,8 +27,8 @@ tpcds_bq.rewrite_schema(filepath_in=config.tpcds_schema_ansi_sql_filepath,
 scale = 1
 
 # run dsdgen with the max number of cpus on this machine
-tpcds_setup.run_dsdgen(cpu=config.cpu_count,
-                       scale=scale)
+ds_setup.run_dsdgen(cpu=config.cpu_count,
+                    scale=scale)
 
 # generate install specific tpc-ds dsdgen bash scripts
 # tpcds_setup.dsdgen_bash_scripts(data_out=config.fp_ds_output_mnt, verbose=True)
