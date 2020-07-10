cc5080985f6b556b9f363122d7308ea2bf087bd2
Colin Dietrich <repos@wildjuniper.com>
2020-07-10 11:09:42 -0700

    Add query statement separators to BQ dataset clone SQL file

--- sc/ds_01A.sql
+++ sc/ds_01A.sql
@@ -7,96 +7,96 @@ select * from `_source_table.dbgen_version`;
 
 create table `_destination_table.customer_address`
 as
-select * from `_source_table.customer_address`
+select * from `_source_table.customer_address`;
 
 create table `_destination_table.customer_demographics`
 as
-select * from `_source_table.customer_demographics`
+select * from `_source_table.customer_demographics`;
 
 create table `_destination_table.date_dim`
 as
-select * from `_source_table.date_dim`
+select * from `_source_table.date_dim`;
 
 create table `_destination_table.warehouse`
 as
-select * from `_source_table.warehouse`
+select * from `_source_table.warehouse`;
 
 create table `_destination_table.ship_mode`
 as
-select * from `_source_table.ship_mode`
+select * from `_source_table.ship_mode`;
 
 create table `_destination_table.time_dim`
 as
-select * from `_source_table.time_dim`
+select * from `_source_table.time_dim`;
 
 create table `_destination_table.reason`
 as
-select * from `_source_table.reason`
+select * from `_source_table.reason`;
 
 create table `_destination_table.income_band`
 as
-select * from `_source_table.income_band`
+select * from `_source_table.income_band`;
 
 create table `_destination_table.item`
 as
-select * from `_source_table.item`
+select * from `_source_table.item`;
 
 create table `_destination_table.store`
 as
-select * from `_source_table.store`
+select * from `_source_table.store`;
 
 create table `_destination_table.call_center`
 as
-select * from `_source_table.call_center`
+select * from `_source_table.call_center`;
 
 create table `_destination_table.customer`
 as
-select * from `_source_table.customer`
+select * from `_source_table.customer`;
 
 create table `_destination_table.web_site`
 as
-select * from `_source_table.web_site`
+select * from `_source_table.web_site`;
 
 create table `_destination_table.store_returns`
 as
-select * from `_source_table.store_returns`
+select * from `_source_table.store_returns`;
 
 create table `_destination_table.household_demographics`
 as
-select * from `_source_table.household_demographics`
+select * from `_source_table.household_demographics`;
 
 create table `_destination_table.web_page`
 as
-select * from `_source_table.web_page`
+select * from `_source_table.web_page`;
 
 create table `_destination_table.promotion`
 as
-select * from `_source_table.promotion`
+select * from `_source_table.promotion`;
 
 create table `_destination_table.catalog_page`
 as
-select * from `_source_table.catalog_page`
+select * from `_source_table.catalog_page`;
 
 create table `_destination_table.inventory`
 as
-select * from `_source_table.inventory`
+select * from `_source_table.inventory`;
 
 create table `_destination_table.catalog_returns`
 as
-select * from `_source_table.catalog_returns`
+select * from `_source_table.catalog_returns`;
 
 create table `_destination_table.web_returns`
 as
-select * from `_source_table.web_returns`
+select * from `_source_table.web_returns`;
 
 create table `_destination_table.web_sales`
 as
-select * from `_source_table.web_sales`
+select * from `_source_table.web_sales`;
 
 create table `_destination_table.catalog_sales`
 as
-select * from `_source_table.catalog_sales`
+select * from `_source_table.catalog_sales`;
 
 create table `_destination_table.store_sales`
 as
-select * from `_source_table.store_sales`
+select * from `_source_table.store_sales`;
